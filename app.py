b2a521273071e0436e3bda35ed5fb73a3194c9ef
ritikchaurasia <85922830+ritikchaurasia@users.noreply.github.com>
2023-09-27 09:00:16 +0530

    Add files via upload
    "SpamGuard NLP: An intelligent Email Spam Classifier using NLP techniques for enhanced email security and efficient spam filtering. Stop unwanted emails with ease.

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pickle 
+import string  
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y=[]
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+    
+    return " ".join(y)
+
+tfidf = pickle.load(open('vectorizer_01.pkl', 'rb'))
+model = pickle.load(open('model_01.pkl', 'rb'))
+
+st.title("Email/SMS Spam Classifier")
+
+input_sms = st.text_input("Enter the message")
+
+if st.button('Predict'):
+    # 1. preprocess
+    transform_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transform_sms])
+    print("Vector input shape:", vector_input.shape)  # Debugging line
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
